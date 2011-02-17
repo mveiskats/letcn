62e46483d16e828d2d3973701609cc6b4b8d4783
sesks <martins.veiskats@gmail.com>
2011-02-17 13:27:17 +0200

    Put in precomputed simplex midpoints

--- lattice.lisp
+++ lattice.lisp
@@ -4,6 +4,8 @@
 (defclass lattice (3d-object)
   ((lattice-values :initarg :lattice-values)))
 
+;;; Cube consists of 6 simplices
+;;; Edge traversal order from (0 0 0) to (1 1 1) identifies them
 (defparameter *travel-orders*
   #(#(0 1 2)
     #(0 2 1)
@@ -12,9 +14,11 @@
     #(2 0 1)
     #(2 1 0)))
 
-;;; Corner coordinates of cube that has been unskewed
-(defparameter *simplex-offsets*
-  (let ((offsets (make-array '(2 2 2 3) :element-type 'float)))
+;;; Precomputed corner coordinates of cube that has been unskewed
+(defparameter *simplex-corners*
+  (let ((corners (make-array '(2 2 2 3)
+                             :element-type 'float
+                             :initial-element 0.0)))
     (dotimes (i 2)
       (dotimes (j 2)
         (dotimes (k 2)
@@ -22,10 +26,31 @@
                  (x (- i s))
                  (y (- j s))
                  (z (- k s)))
-            (setf (aref offsets i j k 0) x
-                  (aref offsets i j k 1) y
-                  (aref offsets i j k 2) z)))))
-    offsets))
+            (setf (aref corners i j k 0) x
+                  (aref corners i j k 1) y
+                  (aref corners i j k 2) z)))))
+    corners))
+
+;;; Precomputed simplex midpoints
+(defparameter *simplex-midpoints*
+  (let ((midpoints (make-array '(6 3)
+                               :element-type 'float
+                               :initial-element 0.0)))
+    (dotimes (simplex 6)
+      (let ((corner (make-array '(3) :element-type 'bit :initial-element 0)))
+        (loop for o across (aref *travel-orders* simplex)
+              do (setf (aref corner o) 1)
+              do (dotimes (n 3)
+                   (incf (aref midpoints simplex n)
+                         (aref *simplex-corners*
+                               (aref corner 0)
+                               (aref corner 1)
+                               (aref corner 2)
+                               n))))
+        (dotimes (n 3)
+          (setf (aref midpoints simplex n)
+                (/ (aref midpoints simplex n) 4)))))
+    midpoints))
 
 ;;; base - base vertex
 ;;; i - travel order index
@@ -39,41 +64,30 @@
 
 (defun make-lattice (w)
   (let ((lattice-values (make-array (list w w w 6) :element-type 'bit)))
-    (dotimes (x w)
-      (dotimes (y w)
-        (dotimes (z w)
-          (dotimes (i 6)
-            (let* ((o (aref *travel-orders* i))
-                   ;; let there be corners
-                   (c0 (make-array 3 :element-type 'fixnum
-                                   :initial-contents (list x y z)))
-                   c1 c2 c3)
-              (setf c1 (copy-seq c0))
-              (incf (aref c1 (aref o 0)))
-              (setf c2 (copy-seq c1))
-              (incf (aref c2 (aref o 1)))
-              (setf c3 (copy-seq c2))
-              (incf (aref c3 (aref o 2)))
-              (let ((v (map '(vector float)
-                            (lambda (a b c d)
-                              (/ (+ a b c d) 4))
-                            (unskew3d-vector c0)
-                            (unskew3d-vector c1)
-                            (unskew3d-vector c2)
-                            (unskew3d-vector c3))))
-              (setf (aref lattice-values x y z i)
-                    (if (< (let* ((w/2 (/ w 2))
-                                  (vx (- (aref v 0) w/2))
-                                  (vy (- (aref v 1) w/2))
-                                  (vz (- (aref v 2) w/2)))
-                             (- (+ (* vx vx) (* vy vy) (* vz vz))
-                                (* w/2 w/2)
-                                -1.0))
-                           (* 10 (noise3d-octaves (/ (aref v 0) 10)
-                                                  (/ (aref v 1) 10)
-                                                  (/ (aref v 2) 10)
-                                                  3 0.25)))
-                        1 0))))))))
+    (dotimes (i w)
+      (dotimes (j w)
+        (dotimes (k w)
+          (let* ((s (* unskew3d-factor (+ i j k)))
+                 (x (- i s))
+                 (y (- j s))
+                 (z (- k s)))
+            (dotimes (simplex 6)
+              (let ((midx (+ x (aref *simplex-midpoints* simplex 0)))
+                    (midy (+ y (aref *simplex-midpoints* simplex 1)))
+                    (midz (+ z (aref *simplex-midpoints* simplex 2))))
+                (setf (aref lattice-values i j k simplex)
+                      (if (< (let* ((w/2 (/ w 2))
+                                    (vx (- midx w/2))
+                                    (vy (- midy w/2))
+                                    (vz (- midz w/2)))
+                               (- (+ (* vx vx) (* vy vy) (* vz vz))
+                                  (* w/2 w/2)
+                                  -1.0))
+                             (* 10 (noise3d-octaves (/ midx 10)
+                                                    (/ midy 10)
+                                                    (/ midz 10)
+                                                    3 0.25)))
+                          1 0))))))))
     (make-instance 'lattice :lattice-values lattice-values)))
 
 (defmethod draw ((l lattice))
