072d23d122b4a397f713575732d9599978076c56
sesks <martins.veiskats@gmail.com>
2011-06-26 07:59:09 +0300

    Code polishing

--- honeycomb.lisp
+++ honeycomb.lisp
@@ -5,162 +5,31 @@
 (defparameter *hc-octree* nil)
 
 
-(defparameter *hc-leaf-size* 4)
-
-;;; TODO: Probably would be a good idea to use
-;;; Morton order (calling it "z-order" will probably be confusing)
-;;; when referencing child nodes and grid cells
-(defclass honeycomb (3d-object)
-  ((cell-values :initarg :cell-values)))
-
-(defclass hc-node ()
-  ((corner :initarg :corner)
-   (size :initarg :size)
-   (samples-visible :initform 0)
-   (query-id :initform nil)))
-
-(defclass hc-partition (hc-node)
-  ((children :initform (make-array '(2 2 2) :initial-element nil))))
-
-(defclass hc-leaf (hc-node)
-  ((list-id :initform nil)))
-
-(defun make-hc-node (grid-offset tree-height)
-  (if (= tree-height 0)
-    (make-hc-leaf grid-offset)
-    (make-hc-partition grid-offset tree-height)))
-
-(defun make-hc-leaf (grid-offset)
-  (make-instance 'hc-leaf
-                 :corner grid-offset
-                 :size *hc-leaf-size*))
-
-(defun make-hc-partition (grid-offset tree-height)
-  (let* ((half-size (* *hc-leaf-size* (expt 2 (1- tree-height))))
-         (size (* half-size 2))
-         (result (make-instance 'hc-partition
-                                :corner grid-offset
-                                :size size)))
-    (with-slots (children) result
-      (doarray (i j k) children
-        (setf (aref children i j k)
-              (make-hc-node (vector+ grid-offset
-                                     (vector* (make-vector i j k) half-size))
-                            (1- tree-height)))))
-    result))
-
-;;; At the moment just polls the results for visibility queries
-(defgeneric post-process (obj))
-
-;;; Draw given node if it has been visible last frame.
-;;; If not, draw bounding box instead
-;;; TODO: should look into making gl:gen-queries return
-;;; multiple values just like gl:gen-lists does
-(defmethod draw :around ((node hc-node))
-  (with-slots (query-id corner samples-visible) node
-    (if (> samples-visible 0)
-      (call-next-method)
-      (progn
-        (when (eq query-id nil) (setf query-id (car (gl:gen-queries 1))))
-        (gl:begin-query :samples-passed query-id)
-        (draw-honeycomb-bounder corner (slot-value node 'size))
-        (gl:end-query :samples-passed)))))
-
-(defmethod post-process :around ((node hc-node))
-  (with-slots (query-id samples-visible) node
-    (if (> samples-visible 0)
-      (call-next-method)
-      ;; Ideally we should check if query is done and
-      ;; give something for cpu to do while it finishes
-      (setf samples-visible (get-query-object-uiv query-id :query-result)))))
-
-(defmethod draw ((node hc-partition))
-  ;; TODO: children should be drawn in correct z-order
-  (with-slots (children) node
-    (doarray (i j k) children
-      (draw (aref children i j k)))))
-
-(defmethod post-process ((node hc-partition))
-  (with-slots (children samples-visible) node
-    (let ((child-samples-visible 0))
-      (doarray (i j k) children
-        (post-process (aref children i j k))
-        (incf child-samples-visible
-              (slot-value (aref children i j k) 'samples-visible)))
-      ;; Update nodes samples-visible to sum of all childrens samples-visible
-      ;; so it will draw bounder next frame instead of individual children
-      (setf samples-visible child-samples-visible))))
-
-(defmethod draw ((node hc-leaf))
-  (with-slots (query-id list-id corner) node
-    (when (eq list-id nil)
-      ;; Got no display list - better generate one
-      (setf list-id (gl:gen-lists 1))
-      (gl:with-new-list (list-id :compile)
-        (let* ((imin (aref corner 0))
-               (jmin (aref corner 1))
-               (kmin (aref corner 2))
-               (imax (+ imin *hc-leaf-size* -1))
-               (jmax (+ jmin *hc-leaf-size* -1))
-               (kmax (+ kmin *hc-leaf-size* -1)))
-          (loop for i from imin to imax
-                do (loop for j from jmin to jmax
-                         do (loop for k from kmin to kmax
-                                  do (unless (zerop (cell-value i j k))
-                                       (case (cell-value i j k)
-                                         (1 (gl:color 0.7 0.3 0.3))
-                                         (2 (gl:color 0.3 0.7 0.3))
-                                         (t (gl:color 0.3 0.3 0.7)))
-                                       (let ((center (grid-to-world (make-vector i j k))))
-                                         (gl:with-pushed-matrix
-                                           (gl:translate (aref center 0) (aref center 1) (aref center 2))
-                                           (dotimes (idx (length +troct-faces+))
-                                             (multiple-value-bind (ii jj kk) (neighbour-cell i j k idx)
-                                               (when (zerop (cell-value ii jj kk))
-                                                 (draw-troct-face idx)))))))))))))
-    ;; TODO: copypasta
-    (when (eq query-id nil) (setf query-id (car (gl:gen-queries 1))))
-    (gl:begin-query :samples-passed query-id)
-    (gl:call-list list-id)
-    (gl:end-query :samples-passed)))
-
-(defmethod post-process ((node hc-leaf))
-  (with-slots (query-id samples-visible) node
-    ;; Ideally we should check if query is done and
-    ;; give something for cpu to do while it finishes
-    ;; TODO: copypasta
-    (setf samples-visible (get-query-object-uiv query-id :query-result))))
-
-(defun cell-value (i j k)
-  (with-slots (cell-values) *honeycomb*
-    (if (array-in-bounds-p cell-values i j k)
-      (aref cell-values i j k)
-      0)))
-
+(defconstant +hc-leaf-size+ 4)
 
 ;;; Helper methods to translate between world position
 ;;; and grid coordinates of cell
-(let* ((g2p #2A((0.5 0.0 0.5)
+(let* ((g2w #2A((0.5 0.0 0.5)
                 (0.5 1.0 0.5)
                 (0.5 0.0 -0.5)))
-       (p2g (invert-matrix g2p)))
-  (defun grid-to-world (g)
-    (matrix*vector g2p g))
-  (defun world-to-grid (p)
-    (map 'vector #'truncate (matrix*vector p2g p))))
+       (w2g (invert-matrix g2w)))
+  (defun grid-to-world (g) (matrix*vector g2w g))
+  (defun world-to-grid (w) (matrix*vector w2g w))
+  (defun world-to-grid-int (w) (map 'vector #'truncate (world-to-grid w))))
 
 ;;; Midpoint of each face scaled by 2 is center of a neighbouring cell
-(defparameter *cell-neighbours*
+(defvar +cell-neighbours+
   (flet ((vertex-sum (face)
            (apply #'mapcar #'+
                   (mapcar (lambda (v) (aref +troct-vertices+ v)) face))))
     (map 'vector
-         (lambda (f) (world-to-grid (vector* (vertex-sum f) (/ 2 (length f)))))
+         (lambda (f) (world-to-grid-int (vector* (vertex-sum f)
+                                                 (/ 2 (length f)))))
          +troct-faces+)))
 
 ;;; Determines indices of a neighbour cell
 (defun neighbour-cell (i j k face)
-  (let ((neighbour (aref *cell-neighbours* face)))
+  (let ((neighbour (aref +cell-neighbours+ face)))
     (values (+ i (aref neighbour 0))
             (+ j (aref neighbour 1))
             (+ k (aref neighbour 2)))))
@@ -169,7 +38,7 @@
 ;;; Finds them by going through all vertices and determining which one
 ;;; is furthest away from plane [axis-a, axis-b] along axis-c
 ;;; TODO: Do this by converting vertices to basis of { axis-a, axis-b, axis-c }
-(defparameter *cell-bounds*
+(defvar +cell-bounds+
   (let* ((axis-i (grid-to-world #(1 0 0)))
          (axis-j (grid-to-world #(0 1 0)))
          (axis-k (grid-to-world #(0 0 1))))
@@ -192,7 +61,7 @@
       (doarray (i j k) corners
         (setf (aref corners i j k)
               (reduce #'vector+
-                      (mapcar #'flip-vector *cell-bounds* (list i j k))
+                      (mapcar #'flip-vector +cell-bounds+ (list i j k))
                       :initial-value (grid-to-world (map 'vector
                                                          #'min-or-max
                                                          (list i j k)))))))
@@ -279,12 +148,12 @@
       (rasterize-honeycomb start end
         (lambda (x y z)
           (let* ((pos (make-vector x y z))
-                 (cell (world-to-grid pos))
+                 (cell (world-to-grid-int pos))
                  (i (aref cell 0))
                  (j (aref cell 1))
                  (k (aref cell 2)))
             (let (face)
-              (when (and (array-in-bounds-p cell-values i j k) 
+              (when (and (array-in-bounds-p cell-values i j k)
                          (not (zerop (aref cell-values i j k)))
                          (setf face (line-troct-intersection start end pos)))
                 (return-from stepper (values pos face))))))))))
@@ -296,13 +165,13 @@
     (draw-troct-face idx)))
 
 (defun remove-cell (center)
-  (let ((cell (world-to-grid center))
+  (let ((cell (world-to-grid-int center))
         (cv (slot-value *honeycomb* 'cell-values)))
     (setf (aref cv (aref cell 0) (aref cell 1) (aref cell 2)) 0
           *scene-modified* t)))
 
 (defun add-cell (center face value)
-  (let ((cell (world-to-grid center))
+  (let ((cell (world-to-grid-int center))
         (cv (slot-value *honeycomb* 'cell-values)))
     (multiple-value-bind (i j k) (neighbour-cell (aref cell 0)
                                                  (aref cell 1)
@@ -311,3 +180,136 @@
       (when (array-in-bounds-p cv i j k)
         (setf (aref cv i j k) value
               *scene-modified* t)))))
+
+(defun cell-value (i j k)
+  (with-slots (cell-values) *honeycomb*
+    (if (array-in-bounds-p cell-values i j k)
+      (aref cell-values i j k)
+      0)))
+
+(defun draw-cell (i j k)
+  (case (cell-value i j k)
+    (1 (gl:color 0.7 0.3 0.3))
+    (2 (gl:color 0.3 0.7 0.3))
+    (t (gl:color 0.3 0.3 0.7)))
+  (let ((center (grid-to-world (make-vector i j k))))
+    (gl:with-pushed-matrix
+      (gl:translate (aref center 0) (aref center 1) (aref center 2))
+      (dotimes (idx (length +troct-faces+))
+        (multiple-value-bind (ii jj kk) (neighbour-cell i j k idx)
+          (when (zerop (cell-value ii jj kk))
+            (draw-troct-face idx)))))))
+
+;;; TODO: Probably would be a good idea to use
+;;; Morton order (calling it "z-order" will probably be confusing)
+;;; when referencing child nodes and grid cells
+(defclass honeycomb (3d-object)
+  ((cell-values :initarg :cell-values)))
+
+(defclass hc-node ()
+  ((corner :initarg :corner)
+   (size :initarg :size)
+   (samples-visible :initform 0)
+   (query-id :initform nil)))
+
+(defclass hc-partition (hc-node)
+  ((children :initform (make-array '(2 2 2) :initial-element nil))))
+
+(defclass hc-leaf (hc-node)
+  ((list-id :initform nil)))
+
+(defun make-hc-node (grid-offset tree-height)
+  (if (= tree-height 0)
+    (make-hc-leaf grid-offset)
+    (make-hc-partition grid-offset tree-height)))
+
+(defun make-hc-leaf (grid-offset)
+  (make-instance 'hc-leaf
+                 :corner grid-offset
+                 :size +hc-leaf-size+))
+
+(defun make-hc-partition (grid-offset tree-height)
+  (let* ((half-size (* +hc-leaf-size+ (expt 2 (1- tree-height))))
+         (size (* half-size 2))
+         (result (make-instance 'hc-partition
+                                :corner grid-offset
+                                :size size)))
+    (with-slots (children) result
+      (doarray (i j k) children
+        (setf (aref children i j k)
+              (make-hc-node (vector+ grid-offset
+                                     (vector* (make-vector i j k) half-size))
+                            (1- tree-height)))))
+    result))
+
+;;; At the moment just polls the results for visibility queries
+(defgeneric post-process (obj))
+
+;;; Draw given node if it has been visible last frame.
+;;; If not, draw bounding box instead
+;;; TODO: should look into making gl:gen-queries return
+;;; multiple values just like gl:gen-lists does
+(defmethod draw :around ((node hc-node))
+  (with-slots (query-id corner samples-visible) node
+    (if (> samples-visible 0)
+      (call-next-method)
+      (progn
+        (when (eq query-id nil) (setf query-id (car (gl:gen-queries 1))))
+        (gl:begin-query :samples-passed query-id)
+        (draw-honeycomb-bounder corner (slot-value node 'size))
+        (gl:end-query :samples-passed)))))
+
+(defmethod post-process :around ((node hc-node))
+  (with-slots (query-id samples-visible) node
+    (if (> samples-visible 0)
+      (call-next-method)
+      ;; Ideally we should check if query is done and
+      ;; give something for cpu to do while it finishes
+      (setf samples-visible (get-query-object-uiv query-id :query-result)))))
+
+(defmethod draw ((node hc-partition))
+  ;; TODO: children should be drawn in correct z-order
+  (with-slots (children) node
+    (doarray (i j k) children
+      (draw (aref children i j k)))))
+
+(defmethod post-process ((node hc-partition))
+  (with-slots (children samples-visible) node
+    (let ((child-samples-visible 0))
+      (doarray (i j k) children
+        (post-process (aref children i j k))
+        (incf child-samples-visible
+              (slot-value (aref children i j k) 'samples-visible)))
+      ;; Update nodes samples-visible to sum of all childrens samples-visible
+      ;; so it will draw bounder next frame instead of individual children
+      (setf samples-visible child-samples-visible))))
+
+(defmethod draw ((node hc-leaf))
+  (with-slots (query-id list-id corner) node
+    (when (eq list-id nil)
+      ;; Got no display list - better generate one
+      (setf list-id (gl:gen-lists 1))
+      (gl:with-new-list (list-id :compile)
+        (let* ((imin (aref corner 0))
+               (jmin (aref corner 1))
+               (kmin (aref corner 2))
+               (imax (+ imin +hc-leaf-size+ -1))
+               (jmax (+ jmin +hc-leaf-size+ -1))
+               (kmax (+ kmin +hc-leaf-size+ -1)))
+          (loop for i from imin to imax
+                do (loop for j from jmin to jmax
+                         do (loop for k from kmin to kmax
+                                  do (unless (zerop (cell-value i j k))
+                                       (draw-cell i j k))))))))
+    ;; TODO: copypasta
+    (when (eq query-id nil) (setf query-id (car (gl:gen-queries 1))))
+    (gl:begin-query :samples-passed query-id)
+    (gl:call-list list-id)
+    (gl:end-query :samples-passed)))
+
+(defmethod post-process ((node hc-leaf))
+  (with-slots (query-id samples-visible) node
+    ;; Ideally we should check if query is done and
+    ;; give something for cpu to do while it finishes
+    ;; TODO: copypasta
+    (setf samples-visible (get-query-object-uiv query-id :query-result))))
